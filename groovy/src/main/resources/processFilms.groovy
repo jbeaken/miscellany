92d892ed3626077cc444414492c3cad96cb775b7
jbeaken <j.beaken@elsevier.com>
2022-06-03 11:21:12 +0000

    Moving script to method

--- groovy/src/main/resources/processFilms.groovy
+++ groovy/src/main/resources/processFilms.groovy
@@ -1,12 +1,10 @@
-package org.mzuri.media
-
 import java.nio.file.*
 import java.util.stream.Stream
 import java.util.stream.Collectors
 
 def dir = "/media/ext/Films"
 
-filenames = []
+def filenames = []
 
 def getTuple(Path path) {
 
@@ -17,27 +15,31 @@ def getTuple(Path path) {
     new Tuple(filename, directoryName)
 }
 
-println "Booting up process films"
+def moveSingleFilmsToDirectories(Path filmsPath) {
 
-try (Stream<Path> stream = Files.list(Paths.get(dir))) {
+    println "Booting up process films"
+
+    try (Stream<Path> stream = Files.list( filmsPath )) {
         filenames = stream
-          .filter(file -> !Files.isDirectory(file))
-          .map(this::getTuple)
-          .collect(Collectors.toSet());
-}
+                .filter(file -> !Files.isDirectory(file))
+                .map(this::getTuple)
+                .collect(Collectors.toSet());
+    }
 
-filenames.each { film ->
+    filenames.each { film ->
 
-    Path directoryToCreate = Path.of(dir, film[1])    
-    Path fileSource = Path.of(dir, film[0])
-    Path fileDestination = Path.of(dir, film[1], film[0])       
+        Path directoryToCreate = Path.of(dir, film[1])
+        Path fileSource = Path.of(dir, film[0])
+        Path fileDestination = Path.of(dir, film[1], film[0])
 
-    println "Directory to create : ${directoryToCreate}"
-    println "fileSource : ${fileSource}"
-    println "fileDestination : ${fileDestination}"
+        println "Directory to create : ${directoryToCreate}"
+        println "fileSource : ${fileSource}"
+        println "fileDestination : ${fileDestination}"
 
 //    Files.createDirectory( directoryToCreate )
 //
 //    Files.move( Paths.get(dir, film[0]) , Paths.get(dir, film[1], film[0]))
+    }
 }
+
 
--- groovy/src/test/groovy/MoveFilmsSpec.groovy
+++ groovy/src/test/groovy/MoveFilmsSpec.groovy
@@ -1,15 +1,51 @@
+import com.google.common.jimfs.Configuration
+import com.google.common.jimfs.Jimfs
 import spock.lang.Specification
 
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.FileSystem
+import java.util.stream.Stream
+
 class MoveFilmsSpec extends Specification {
+
+  def setup() {
+    // For a simple file system with Unix-style paths and behavior:
+    FileSystem fs = Jimfs.newFileSystem(Configuration.unix());
+
+    Path foo = fs.getPath("/blah");
+    Files.createDirectory(foo);
+
+//    Path foo = fs.getPath("/media/ext/Films/test");
+//    Files.createDirectory(Path.of("/media"));
+//    Files.createDirectory(Path.of("/media/ext"));
+//    Files.createDirectory(Path.of("/media/ext/Films"));
+  }
 
   def "run move single movie files into own directories"() {
     given:
     File file1 = new File(MoveFilmsSpec.class.getResource("processFilms.groovy").toURI())
 
+    Path filmPath = Path.of("/media/ext/Films")
+
     GroovyShell groovyShell = new GroovyShell()
 
     def script = groovyShell.parse(file1)
 
-    script.run()
+    script.moveSingleFilmsToDirectories(filmPath)
+  }
+
+  def "Should create a file on a file system"() {
+    given:
+
+    FileSystem fileSystem = Jimfs.newFileSystem(Configuration.unix());
+    String fileName = "newFile.txt";
+    Path pathToStore = fileSystem.getPath("");
+    Path filePath = pathToStore.resolve(fileName);
+    Files.createFile(filePath);
+
+    expect:
+    Files.exists( pathToStore.resolve(fileName) ) == true
+    Files.exists( Path.of("/blah" )) == true
   }
 }  
